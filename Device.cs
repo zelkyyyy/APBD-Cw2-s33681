496a9d5d1c1b50951ffc4ab643771d41456e1421
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-03-22 00:41:29 +0100

    Zmiana nazwy pliku oraz klasy

--- Device.cs
+++ Device.cs
@@ -1,13 +1,13 @@
 namespace APBD_Cw2_s33681;
 
-public class Sprzet
+public class Device
 {
     private static int IdCounter;
     public int Id { get; set; }
     public string Name { get; set; }
     public bool IsAvailable { get; set; }
 
-    public Sprzet(string name)
+    public Device(string name)
     {
         Id = IdCounter++;
         this.Name = name;
